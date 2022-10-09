fef43c4dc6d3eb41ec71e2e503a7d5160a02b17d
AssaultWolf <115419890+AssaultWolf@users.noreply.github.com>
2022-10-10 01:24:02 +0700

    1

---
+++ demo/src/main/java/com/example/sale/Entity/ProductRate.java
@@ -0,0 +1,51 @@
+package com.example.sale.Entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ProductRates")
+public class ProductRate implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Id")
+	private Integer id;
+	
+	@Column(name = "Rating")
+	private Integer rating;
+	
+	@Column(name = "ProductId") 
+	private Integer productId;
+	
+	@Column(name = "Username")
+	private String username;
+	
+	@ManyToOne
+	@JoinColumn(name = "ProductId")
+	private Product product;
+	
+	@ManyToOne
+	@JoinColumn(name = "Username")
+	private User user;
+}
